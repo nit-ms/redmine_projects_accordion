ea3dcba25e5d34b3a3b512a1e61a5dee07deb288
Felipe V. Rigo <felipe.rigo@ufms.br>
2019-04-11 10:39:14 -0400

    Changes made by Agetic-UFMS team along the years

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
-Redmine::Plugin.register :redmine_projects_accordion do
-  name 'Redmine Projects Accordion plugin'
-  author 'Reuben Mallaby'
-  description 'This is a Redmine plugin which will turn the projects page into an accordion view using jQuery UI accordion code'
+Redmine::Plugin.register :redmine_nti_accordion do
+  name 'Redmine Projects Accordion plugin NTI Modifications'
+  author 'Vinicius Victorio'
+  description 'Este plugin foi alterado para as atender as necessidades do NTI'
   version '0.1.0'
   url 'http://reuben.mallaby.me'
   author_url 'http://reuben.mallaby.me'
@@ -11,7 +11,7 @@ class RedmineProjectsAccordionListener < Redmine::Hook::ViewListener
 
   # Adds javascript and stylesheet tags
   def view_layouts_base_html_head(context)
-    stylesheet_link_tag('redmine_projects_accordion', :plugin => :redmine_projects_accordion)
+    stylesheet_link_tag('redmine_projects_accordion', :plugin => :redmine_nti_accordion)
   end
   
 end
