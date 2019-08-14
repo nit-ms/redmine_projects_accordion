aafafc2c5ef8c8b6126091837e36cea30b0485e3
MatheusLopo <matheuslopo22@gmail.com>
2019-08-14 08:08:04 -0400

    Modificação css

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-Redmine::Plugin.register :redmine_Agetic_accordion do
+Redmine::Plugin.register :redmine_projects_accordion do
   name 'Redmine Projects Accordion plugin NTI Modifications'
   author 'Vinicius Victorio'
   description 'Este plugin foi alterado para as atender as necessidades do NTI'
@@ -11,7 +11,7 @@ class RedmineProjectsAccordionListener < Redmine::Hook::ViewListener
 
   # Adds javascript and stylesheet tags
   def view_layouts_base_html_head(context)
-    stylesheet_link_tag('redmine_projects_accordion', :plugin => :redmine_nti_accordion)
+    stylesheet_link_tag('redmine_projects_accordion', :plugin => :redmine_projects_accordion)
   end
   
 end
